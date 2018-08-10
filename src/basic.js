97ce67a2860d0e6bc06a717f705858a9604592e0
Anjum <anjumqureshi000@gmail.com>
2018-08-10 19:17:25 +0530

    added averageOF

--- src/basic.js
+++ src/basic.js
@@ -84,6 +84,9 @@ const greatestOf = function (number1, number2, number2) {
   return number3;
 }
 
+const averageOf = function (number1, number2, number3) {
+  return (number1 + number2 + number3) / 3;
+}
 /*
   averageOf
   This function returns the average of three numbers
