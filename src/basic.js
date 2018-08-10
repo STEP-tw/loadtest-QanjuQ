add67b4d5bec72ee4d8a166f18d2b842e83ace54
Anjum <anjumqureshi000@gmail.com>
2018-08-10 19:12:35 +0530

    added gcd

--- src/basic.js
+++ src/basic.js
@@ -40,6 +40,13 @@ const cube = function (number) {
   This function returns the greatest common divisor of any two numbers
 */
 
+const gcd = function (firstNumber, secondNumber) {
+  for (let divisor = firstNumber; divisor >= 1; divisor--) {
+    if (firstNumber % divisor == 0 && secondNumber % divisor == 0) {
+      return divisor;
+    }
+  }
+}
 
 /*
   lcm
